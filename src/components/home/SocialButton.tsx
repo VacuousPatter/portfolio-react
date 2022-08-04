d38d37936aa7548dc2bf96b1a2de30b9f035aee2
Maycon Jesus <mayconjesus07@gmail.com>
2022-08-04 07:52:43 -0300

    fix: background color

--- src/components/home/SocialButton.tsx
+++ src/components/home/SocialButton.tsx
@@ -24,6 +24,27 @@ export default function SocialButton(props: Props) {
     const buttonElement = useRef<HTMLAnchorElement>(null);
     const actualTheme = useRecoilValue(actualThemeState);
 
+    if (buttonElement.current) {
+        const backgroundColor =
+            props.backgroundColor || actualTheme.palette.primary.main;
+        buttonElement.current.style.setProperty(
+            '--background-color',
+            props.showBackgroundOnlyOnHover ? 'transparent' : backgroundColor
+        );
+        buttonElement.current.style.setProperty(
+            '--onhover-background-color',
+            props.onHoverBackgroundColor || backgroundColor
+        );
+
+        const textColor =
+            props.textColor || actualTheme.palette.primary.contrastText;
+        buttonElement.current.style.setProperty('--text-color', textColor);
+        buttonElement.current.style.setProperty(
+            '--onhover-text-color',
+            props.onHoverTextColor || textColor
+        );
+    }
+
     useEffect(() => {
         if (!buttonElement.current) return;
         const backgroundColor =
